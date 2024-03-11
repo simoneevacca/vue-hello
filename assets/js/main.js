6dd0dc7c21a498338b2998057976bd031eafa500
simoneevacca <v.simone.vacca@gmail.com>
2024-03-11 14:25:01 +0100

    add button for change image size

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,27 @@ createApp(
         data() {
             return {
                 message: 'Hello Word!',
-                image: "https://picsum.photos/500/500/"
+                image: `https://picsum.photos/100/100/`,
+                counter: 100,
+            }
+        },
+
+        mounted() {
+            console.log(this.image);
+            this.counter = this.counter;
+
+        },
+
+        methods: {
+            send() {
+                if (this.counter < 100){
+                    this.counter = 100
+                }
+                console.log('reload image size');
+                this.image = `https://picsum.photos/${this.counter}/${this.counter}/`
             }
         }
+
     }
 ).mount('#app')
 
